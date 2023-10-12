ee5fa05f4ea832a10b4848d5e504878fd9816fb3
youssefpasha <youssefbashafcis@gmail.com>
2023-10-12 21:12:54 +0300

    authntiaction & database by clerk & convex

--- app/(LandingPage)/_components/navbar.tsx
+++ app/(LandingPage)/_components/navbar.tsx
@@ -1,11 +1,18 @@
 "use client";
-import { useScrollTop } from "@/Hooks/user-scroll-top";
-import { cn } from "@/lib/utils";
 import React from "react";
-import Logo from "./logo";
+import Link from "next/link";
+import { useScrollTop } from "@/Hooks/user-scroll-top";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { ModeToggle } from "@/components/mode-toggle";
 
+import Logo from "./logo";
+
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +24,27 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get NotionII free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href="documents">Enter NotionII</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,16 +33,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="notionII-theme-2"
-        >
-          {children}
-        </ThemeProvider>
+      <body className={cn(inter.className)}>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="notionII-theme-2"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
